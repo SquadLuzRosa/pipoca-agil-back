b6ff52c2ff692c79e973f0aa59e8b24832cbe6c2
thiagoferreirapy <tf938383@gmail.com>
2023-12-01 11:17:32 -0300

    init: initializing the project structure/ included api

---
+++ src/tests/userRegisterTest/UserRegister.spec.js
@@ -0,0 +1,27 @@
+import {describe, expect, test, it} from '@jest/globals';
+import {userDataValidation} from '../../validations/user.validation'
+
+function sun(x, y) {
+    return x + y;
+};
+
+describe("Creating a new user, Success case!", ()=>{
+    it("It must be possible to create a new user", async () => {
+       
+        const req = {
+            name: "thiago silva",
+            email: "jesttest@gmail.com",
+            phone: "61994162080",
+            dateOfBirth: "2002/08/27",
+            password: "12456#tT",
+            confirmPassword: "12456#tT"
+        };
+
+        console.log(req);
+        const res = {};
+        const user = await userDataValidation(req);
+
+
+        expect(user).toEqual(true);
+    })
+});
